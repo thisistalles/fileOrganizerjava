457560bea1762ae351927e199776596c1cd69bda
MN <tgtallles362@gmail.com>
2024-05-22 09:13:43 -0300

    adicionado maven ao projeto

--- fileOrganizer/src/main/java/organizerfile/Main.java
+++ fileOrganizer/src/main/java/organizerfile/Main.java
@@ -1,12 +1,10 @@
 package organizerfile;
 
-
 public class Main {
     public static void main(String[] args) {
 
         Organizer organizer = new Organizer();
-
-
+        
         organizer.fileCheck();
 
 
